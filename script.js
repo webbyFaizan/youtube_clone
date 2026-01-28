ae4490579e019131e4c24292d80cde98235035c2
Faizan Ansari <faizanansari989293@gmail.com>
2026-01-28 14:24:49 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const menuButton = document.querySelectorAll(".menu-button");
-const menuButton = document.querySelectorAll(".menu-button");
+const screenOverlay = document.querySelector(".screen-overlay");
+const themeButton = document.querySelector(".theme-button");
+
+themeButton.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark-mode");
+});
 
 menuButton.forEach(button =>{
     button.addEventListener("click",()=>{
         document.body.classList.toggle("sidebar-hidden");   
     });
 });
+
+screenOverlay.addEventListener("click", ()=>{
+    document.body.classList.toggle("sidebar-hidden");
+});
+
+if(window.innerHeight >= 768){
+    document.body.classList.remove("sidebar-hidden");
+}
